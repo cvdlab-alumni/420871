046f0992994013b91cde55253fa0054eadd7c671
bit111 <biagio.t87@gmail.com>
2014-05-17 16:43:42 +0200

    homework3

---
+++ 2014-05-16/python/exercise3.py
@@ -0,0 +1,35 @@
+# Exercise 3
+
+""" progressive refinement of a block diagram """
+from pyplasm import *
+from scipy import *
+import os,sys
+""" import modules from larcc/lib """
+sys.path.insert(0, '/home/biagio/lar-cc/lib/py/')
+from lar2psm import *
+from simplexn import *
+from larcc import *
+from largrid import *
+from mapper import *
+from boolean import *
+
+from sysml import *
+
+def automate_merging(master, mergingWith, diagram):
+	V,CV = master
+	sorting = sorted(mergingWith, reverse=True)
+	for k in range(len)(mergingWith):
+		if mergingWith[k] in CV:
+			master = diagram2cell(master, diagram, mergingWith[k])
+
+
+def automate_removing(master, removingFrom):
+	V,CV = master
+	sorting = sorted(removingFrom, reverse=False)
+	for n in range(len)(removingFrom):
+		if removingFrom[n] in CV:
+			# memorizzo il valore da rimuovere in una lista temporanea
+			2remove = [removingFrom[n]]
+			master = V,[cell for k,cell in enumerate(CV) if not (k in 2remove)]
+	return master
+		
